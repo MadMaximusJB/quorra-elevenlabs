86b3fb23522753e9829efe2e817365efdc13cf75
MadMaximusJB <62156106+MadMaximusJB@users.noreply.github.com>
2024-06-12 22:49:46 -0400

    Updated text_processing.py to use GPT-4o

--- utils/text_processing.py
+++ utils/text_processing.py
@@ -1,26 +1,25 @@
-import openai
+from openai import OpenAI
 from config import OPENAI_API_KEY
-
-openai.api_key = OPENAI_API_KEY
-
-# Define the initial instructions for the assistant
-INITIAL_PROMPT = (
-    "You are an intelligent voice assistant. You will respond to user queries "
-    "in a friendly and helpful manner. Always be concise and provide useful information. Your name is Quorra."
-)
 
 def process_text_with_gpt4o(text):
     try:
+        ## Set the API key
+        client = OpenAI(api_key=OPENAI_API_KEY)
+        
         # Combine the initial instructions with the user input
-        prompt = INITIAL_PROMPT + "\n\nUser: " + text + "\nAssistant:"
+        prompt = text 
         
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=150
+        MODEL="gpt-4o"
+
+        completion = client.chat.completions.create(
+        model=MODEL,
+        messages=[
+        {"role": "system", "content": "You are an intelligent voice assistant. You will respond to user queries "
+        "in a friendly and helpful manner. Always be concise and provide useful information. Your name is Quorra, you are a young to middle aged female."},
+        {"role": "user", "content": prompt}
+        ]
         )
-        
-        return response.choices[0].text.strip()
+        return completion.choices[0].message.content.strip()
     except Exception as e:
         print(f"Error processing text with GPT-4o: {e}")
         return None
