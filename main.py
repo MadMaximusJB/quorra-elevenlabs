52c97eae4dadbce657737ee3eea8e167b1c254be
MadMaximusJB <62156106+MadMaximusJB@users.noreply.github.com>
2024-06-12 21:59:59 -0400

    Updated components to v1.0

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print("Exiting voice assistant.")
+        print("Exiting Quorra.")
--- utils/text_to_speech.py
+++ utils/text_to_speech.py
@@ -1,11 +1,17 @@
-from elevenlabs import generate, play
+from elevenlabs.client import ElevenLabs
+from elevenlabs import play, save, stream, Voice, VoiceSettings
 from config import ELEVENLABS_API_KEY
 
 def text_to_speech_with_elevenlabs(text, voice_id="default"):
-    try:
-        # Generate speech from text using the specified voice
-        audio = generate(text, api_key=ELEVENLABS_API_KEY, voice=voice_id)
-        # Play the generated audio
-        play(audio)
-    except Exception as e:
-        print(f"Error converting text to speech: {e}")
+  try:
+
+    #Create ElevenLabs client
+    client = ElevenLabs(api_key=ELEVENLABS_API_KEY)
+
+    # Generate speech from text using the specified voice
+    audio_stream = client.generate(text=text, voice=voice_id, stream=True)
+    
+    # Stream the audio
+    stream(audio_stream)
+  except Exception as e:
+    print(f"Error converting text to speech: {e}")
